f86f7fab327aa920f4c9de1b2e526c23e6d24f23
Brad Fritz <brad@fewerhassles.com>
2012-08-13 09:11:53 -0400

    first cut at case classes to represent city and country tables

---
+++ src/main/scala/City.scala
@@ -0,0 +1,3 @@
+package org.indyscala.slick
+
+case class City(id: Int, name: String, countrycode: String, district: String, population: Int)
---
+++ src/main/scala/Country.scala
@@ -0,0 +1,18 @@
+package org.indyscala.slick
+
+case class Country(
+  code: String,
+  name: String,
+  continent: String,
+  region: String,
+  surfacearea: Double,
+  indepyear: Int,
+  population: Int,
+  lifeexpectancy: Double,
+  gnp: Double,
+  gnpold: Double,
+  localname: String,
+  headofstate: String,
+  capital: Int,
+  code2: String
+)
